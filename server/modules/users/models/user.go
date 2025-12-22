a7790ed8872f7f618f0f54378dbb1d50c1585be9
zshstacks <uns4d123@gmail.com>
2025-12-23 00:15:36 +0200

    user model json fix

--- server/modules/users/models/user.go
+++ server/modules/users/models/user.go
@@ -6,9 +6,9 @@ import (
 
 type User struct {
 	gorm.Model
-	UniqueID              string `gorm:"uniqueIndex;size:12;not null"`
-	Email                 string `gorm:"uniqueIndex;size:40;not null"`
-	Username              string `gorm:"size:30"`
+	UniqueID              string `gorm:"uniqueIndex;size:12;not null" json:"uniqueID"`
+	Email                 string `gorm:"uniqueIndex;size:40;not null" json:"email"`
+	Username              string `gorm:"size:30" json:"username"`
 	Password              string `gorm:"not null" json:"-"`
 	IsEmailConfirmed      bool   `gorm:"default:false"`
 	EmailConfirmationCode string `gorm:"size:64" json:"-"`
